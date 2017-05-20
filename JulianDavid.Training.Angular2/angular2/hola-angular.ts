cf7caadf904b2bc2a63eaf58602e94f0368264d7
julian-arroyave <julian.arroyave@newshore.es>
2017-05-20 07:18:14 -0500

    [Agular2] Library

---
+++ JulianDavid.Training.Angular2/angular2/hola-angular.ts
@@ -0,0 +1,16 @@
+﻿import { Component } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+@Component({
+    selector: "hola-angular",
+    template:"<h1>{{saludo}}</h1>"
+})
+
+class HolaAngularComponent {
+    saludo: string;
+    constructor() {
+        this.saludo = '¡Hola Angular 2!';
+    }
+}
+
+platformBrowserDynamic().bootstrapModule(HolaAngularComponent);//Componente es bootstrap﻿
